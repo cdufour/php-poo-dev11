b14ddb52ec3d3735e2c74e4f63476b499ae4d4e0
cdufour <opusidea@gmail.com>
2017-02-16 17:20:49 +0100

    jour 4

--- TP/rencontres-sportives-2/formation-php-poo.sql
+++ TP/rencontres-sportives-2/formation-php-poo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 15 Février 2017 à 14:30
+-- Généré le :  Jeu 16 Février 2017 à 17:17
 -- Version du serveur :  10.1.21-MariaDB
 -- Version de PHP :  5.6.30
 
@@ -19,6 +19,27 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `formation-php-poo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `competition`
+--
+
+CREATE TABLE `competition` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `competition`
+--
+
+INSERT INTO `competition` (`id`, `nom`) VALUES
+(1, 'Amical'),
+(2, 'Ligue des Champions'),
+(3, 'Ligue 1'),
+(4, 'Ligue 2');
 
 -- --------------------------------------------------------
 
@@ -39,7 +60,7 @@ CREATE TABLE `equipe` (
 
 INSERT INTO `equipe` (`id`, `nom`, `logo`, `annee_de_creation`) VALUES
 (1, 'Paris Saint Germain', '', 1970),
-(2, 'Juventus', '', 1897),
+(2, 'Juventus', 'http://www.juventus.com/pics/layout/juventus_logo.png', 1897),
 (3, 'Arsenal', '', 1892),
 (4, 'Chelsea', '', 1950),
 (5, 'Leicester', '', 1936),
@@ -47,6 +68,28 @@ INSERT INTO `equipe` (`id`, `nom`, `logo`, `annee_de_creation`) VALUES
 (7, 'Benfica Lisbonne', '', 1890),
 (8, 'FC Porto', '', 1906),
 (9, 'FC Porto', '', 1906);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `equipe` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `joueur`
+--
+
+INSERT INTO `joueur` (`id`, `nom`, `prenom`, `equipe`) VALUES
+(1, 'Pogba', 'Paul', 2),
+(2, 'Marchisio', 'Claudio', 2),
+(3, 'Gianluigi', 'Buffon', 2);
 
 -- --------------------------------------------------------
 
@@ -70,19 +113,36 @@ CREATE TABLE `rencontre` (
 --
 
 INSERT INTO `rencontre` (`id`, `equipe1`, `equipe2`, `score1`, `score2`, `date`, `lieu`, `competition`) VALUES
-(3, 2, 1, 0, 0, '2016-08-14 18:00:00', 'Paris', 2),
-(4, 4, 3, 2, 1, '2016-09-13 00:00:00', 'Turin', 3),
-(5, 6, 7, 4, 3, '2017-01-09 00:00:00', 'San Siro', 2),
-(6, 1, 4, 0, 0, '2012-02-02 20:45:00', 'Londres', 4);
+(6, 1, 4, 0, 0, '2012-02-02 20:45:00', 'Londres', 4),
+(8, 4, 8, 4, 2, '0000-00-00 00:00:00', 'Milan', 2),
+(9, 2, 1, 6, 1, '0000-00-00 00:00:00', 'Turin', 1),
+(10, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0),
+(11, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0),
+(12, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0),
+(13, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0),
+(14, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0),
+(15, 0, 0, 0, 0, '0000-00-00 00:00:00', '', 0);
 
 --
 -- Index pour les tables exportées
 --
 
 --
+-- Index pour la table `competition`
+--
+ALTER TABLE `competition`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `equipe`
 --
 ALTER TABLE `equipe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,15 +156,25 @@ ALTER TABLE `rencontre`
 --
 
 --
+-- AUTO_INCREMENT pour la table `competition`
+--
+ALTER TABLE `competition`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT pour la table `equipe`
 --
 ALTER TABLE `equipe`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT pour la table `rencontre`
 --
 ALTER TABLE `rencontre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
